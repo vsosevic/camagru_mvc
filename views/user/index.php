b68632006571efd2f9817bf1418d38c9d0283693
Vladimir Sosevich <sosevich.v@gmail.com>
2018-03-08 17:51:00 +0200

    Redirect to login page if not logged

--- views/user/index.php
+++ views/user/index.php
@@ -1,2 +1,6 @@
-<?php
-echo 'myCamagru page';
+<?php if(empty($_SESSION['logged'])): ?>
+    <script type="text/javascript">
+        window.location.href = '/login';
+    </script>
+<?php endif; ?>
+<span>My camagru page</span>
